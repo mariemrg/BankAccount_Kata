9e0e68ef12f1d3645d6e544c70f8ac9e7c440707
mariemrg <60475326+mariemrg@users.noreply.github.com>
2022-11-10 00:01:03 +0100

    Update AcoountTest.java

--- src/test/java/bank/Model/AcoountTest.java
+++ src/test/java/bank/Model/AcoountTest.java
@@ -11,20 +11,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class AcoountTest {
 
     @Test
-    // not_be_able_to_create_account_with_null_initial_amount
     public void acountTestWithNullParameter() {
         assertThrows(IllegalArgumentException.class, () -> new Account(null));
     }
 
     @Test
-    // Not_be_able_to_negative_initial_amounts
     public void accountTestWithNagtiveIntialAmount(){
         assertThrows(IllegalArgumentException.class, () -> new Account(new BigDecimal("-765")));
     }
 
 
     @Test
-    // Not_be_able_to_withDraw_negative_amounts
     public void withDrawTestWithNegativeAmount() {
         // given
         Account account = new Account(new BigDecimal("1000"));
@@ -33,7 +30,6 @@ public class AcoountTest {
     }
 
     @Test
-   // Not_be_able_to_deposit_negative_amounts
     public void withDrawTestWithNegativeWithDraw() {
         // given
         Account account = new Account(new BigDecimal("100"));
@@ -42,7 +38,6 @@ public class AcoountTest {
     }
 
     @Test
-    // Get_1000_balance_given_initial_amount_800_and_deposit_200
    public void accountTestCountBalanceWithDepositAmount() {
         // given
         Account account = new Account(new BigDecimal("800"));
@@ -53,7 +48,6 @@ public class AcoountTest {
     }
 
     @Test
-    // Get_600_balance_given_initial_amount_800_and_withDraw_200
     public void accountTestCountBalanceWithDrawAmount() {
         // given
         Account account = new Account(new BigDecimal("800"));
