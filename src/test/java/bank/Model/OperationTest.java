245139a02f1a711bca595144146b00502bafd031
mariemrg <60475326+mariemrg@users.noreply.github.com>
2022-11-10 00:01:45 +0100

    Update OperationTest.java

--- src/test/java/bank/Model/OperationTest.java
+++ src/test/java/bank/Model/OperationTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class OperationTest {
 
     @Test
-    //can_not_be_able_to_create_invalid_operation
     public void OperationTestWithInvalidParamertes() {
         assertThrows(Exception.class, () -> new Operation(null, null, null));
     }
